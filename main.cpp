6a83e9bbe7b45a6b0532c77bf8e3ef443136df51
jjomiste <jjomiste@gmail.com>
2016-02-08 23:23:54 +0100

    Draft triangle

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@
 
 using namespace std;
 
-int main()
-{
-  std::cout << "Hello World!";
+void printBieeeeenorOoh(const bool result){
+	if (result){
+
+	}
+}
+
+int main(){
+ const Rectangle rect(4.0,2.0);
+ const Triangle tri(4.0,2.0);
 }
