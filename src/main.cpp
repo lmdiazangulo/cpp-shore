aeb4fe1597b610f3677647c978e5353f91e5d925
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2016-02-15 23:23:05 +0100

    Creada lista de figuras.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 // my first program in C++
 #include <iostream>
 #include <math.h>
+#include <vector>
 
-#include "Rectangle.h"
 #include "Circle.h"
+#include "Octagon.h"
+#include "Rectangle.h"
+#include "Triangle.h"
+
 
 using namespace std;
 
@@ -15,17 +19,33 @@ void printBieeeenOrOhh(const bool result) {
 	}
 }
 
+double computeArea(const vector<const Figure*>& rhs) {
+	double res = 0.0;
+	for (unsigned int i = 0; i < rhs.size(); i++) {
+		res += rhs[i]->getArea();
+	}
+	return res;
+}
+
 int main() {
 	const Rectangle rect(4.0, 2.0);
 	printBieeeenOrOhh(rect.getArea() == 8.0);
 
-//	const Triangle tri(4.0, 2.0);
-//	printBieeeenOrOhh(tri.getArea() == 4.0);
-
-	const Octagon octo()
+	const Triangle tri(4.0, 2.0);
+	printBieeeenOrOhh(tri.getArea() == 4.0);
 
 	const Circle circ(5.0);
 	printBieeeenOrOhh(circ.getArea() == M_PI * pow(5.0,2));
+
+	vector<const Figure*> figureList;
+
+	figureList.push_back(new Rectangle(4.0, 2.0));
+	figureList.push_back(new Rectangle(5.0, 3.0));
+	figureList.push_back(new Circle(2.0));
+	figureList.push_back(new Octagon(3.0));
+//	figureList.push_back(new Triangle(5.0,2.0));
+
+	cout << computeArea(figureList) << endl;
 
 	return 0;
 
